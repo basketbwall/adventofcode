7e239e9b15fb81006f1ba8e108b193a8f3c0a3d4
William Huang <willh4k@gmail.com>
2022-12-03 13:13:47 -0500

    committing day 1 part 2

--- day2.py
+++ day2.py
@@ -1,15 +1,18 @@
 with open('input.txt') as f:
     lines = f.readlines()
     calorie = 0
-    res = 0
+    res = []
     for line in lines:
         if line == "\n":
             #update result
-            res = max(res, calorie)
+            res.append(calorie)
             #reset calorie
             calorie = 0
         else:
             #increment calorie
             calorie += int(line.strip())
-    
-    print(res)
+    sorted = sorted(res)
+    ans = 0
+    for i in range(len(sorted)-1, len(sorted)-4, -1):
+        ans += sorted[i]
+    print(ans)
